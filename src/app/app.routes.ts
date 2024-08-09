71a1e136913051bee5c4ab4cdffb2fc1df2ab7c9
Leo Carovana <leonelcarovana11@gmail.com>
2024-08-08 21:13:46 -0300

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './routing/start/start.component.js';
+import { FirstComponent } from './routing/first/first.component.js';
+import { SecondComponent } from './routing/second/second.component.js';
+import { PageNotFoundComponent } from './routing/page-not-found/page-not-found.component.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'start',
+    component:StartComponent
+  }
+  ,
+  {
+    path:'first',
+    component:FirstComponent
+  }
+  ,
+  {
+    path:'second',
+    component:SecondComponent
+  }
+  ,
+  {
+    path:'',
+    redirectTo:'start',
+    pathMatch: 'full'
+  }
+  ,
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  }
+
+];
