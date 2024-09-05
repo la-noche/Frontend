4ac8f4499ec9fbcd5e23863b3f2f03479e33259c
Santiago <santiagocapriotti@gmail.com>
2024-09-05 17:09:34 -0300

    CREACION DE CRUD REGION Component region Component region-form Service region Interface region Routes region

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './routing/start/start.component.js';
-import { Login } from './routing/login/login.component.js';
+import { LoginComponent } from './routing/login/login.component.js';
 import { GameTypesComponent } from './routing/game-types/game-types.component.js';
 import { GameTypeFormComponent } from './routing/game-type-form/game-type-form.component.js';
 import { GameComponent } from './routing/game/game.component.js';
 import { GameFormComponent } from './routing/game-form/game-form.component.js';
 import { authGuard } from './guards/auth.guard.js';
+import { RegionComponent } from './routing/region/region.component.js';
+import { RegionFormComponent } from './routing/region-form/region-form.component.js';
 
 export const routes: Routes = [
   {
     path: 'login',
-    component: Login,                   /* FALTA DESARROLLAR EL CRUD DE LOGIN EN COMPONENT */
+    component: LoginComponent,                   /* FALTA DESARROLLAR EL CRUD DE LOGIN EN COMPONENT */
     title: 'Inicio de sesión',
   },
   {
@@ -37,7 +39,17 @@ export const routes: Routes = [
   {
     path: 'game-form/:id',
     component: GameFormComponent,
-    title: 'Formulario de Juego',
+    title: 'Formulario de Juegos',
+  },
+  {
+  path: 'region',
+  component: RegionComponent,
+  title: 'Regiones',
+  },
+  {
+  path: 'region-form/:id',
+  component: RegionFormComponent,
+  title: 'Formulario de Regiones',
   },
   {
     path: '**',
---
+++ src/app/routing/region-form/region-form.component.ts
@@ -0,0 +1,147 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RegionService } from '../../services/region.service.js';
+import {
+  ActivatedRoute,
+  Router,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { RegionInterface } from '../../interfaces/region.interface.js';
+import { DropdownModule } from 'primeng/dropdown';
+
+@Component({
+  selector: 'app-region-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    ButtonModule,
+    RouterModule,
+    InputTextModule,
+    CardModule,
+    RouterOutlet,
+    DropdownModule,
+  ],
+  templateUrl: './region-form.component.html',
+  styleUrl: './region-form.component.css'
+})
+
+export class RegionFormComponent implements OnInit {
+  formRegion!: FormGroup;
+  isSaveInProgress: boolean = false;
+  edit: boolean = false;
+  regionList: RegionInterface[] = [];
+  regionLoaded: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private regionService: RegionService,
+    private activatedRoute: ActivatedRoute,
+    private messageService: MessageService,
+    private router: Router
+  ) {
+    this.formRegion = this.fb.group({
+      id: [null],
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id !== 'new') {
+      this.edit = true;
+      this.getRegionById(+id!);
+    }
+  }
+
+  getRegionById(id: number) {
+    this.regionService.getRegionById(id).subscribe({
+      next: (foundRegion) => {
+        this.formRegion.patchValue(foundRegion);
+      },
+      error: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No encontrado',
+        });
+        this.router.navigateByUrl('/');
+      },
+    });
+  }
+
+  createRegion() {
+    if (this.formRegion.invalid) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Por favor, complete los campos',
+      });
+      return;
+    }
+    this.isSaveInProgress = true;
+    this.regionService.createRegion(this.formRegion.value).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Creada',
+          detail: 'Región guardada correctamente',
+        });
+        this.isSaveInProgress = false;
+        this.router.navigateByUrl('/region');
+      },
+      error: () => {
+        this.isSaveInProgress = false;
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Revise los campos e intente nuevamente',
+        });
+      },
+    });
+    console.log(this.formRegion.value);
+  }
+
+  updateRegion() {
+    if (this.formRegion.invalid) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Revise los campos e intente nuevamente',
+      });
+      return;
+    }
+    this.isSaveInProgress = true;
+    this.regionService.updateRegion(this.formRegion.value).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Guardado',
+          detail: 'Región actualizada correctamente',
+        });
+        this.isSaveInProgress = false;
+        this.router.navigateByUrl('/region');
+      },
+      error: () => {
+        this.isSaveInProgress = false;
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Revise los campos e intente nuevamente',
+        });
+      },
+    });
+  }
+}
