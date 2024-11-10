66dd6e139a53f333b1e4ef75f453207b4391ca56
Santiago <santiagocapriotti@gmail.com>
2024-11-10 16:16:11 -0300

    Se arreglaron bordes en las interfaces y diseño del home

--- src/app/routing/start/start.component.ts
+++ src/app/routing/start/start.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-start',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, RippleModule],
   templateUrl: './start.component.html',
   styleUrl: './start.component.css'
 })
